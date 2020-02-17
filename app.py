6f11312a59faedc4f2da458f9dc4a1270175c7a4
Ali Karimi <alikarimi@Alis-MacBook-Pro.local>
2020-02-17 17:08:18 -0500

    fix cors error

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import requests
 from flask import Flask
 from elasticsearch import Elasticsearch
+from rapidjson import dumps
+
+from scripts.utils import get_headers
 
 app = Flask(__name__)
 es = Elasticsearch(['http://elastic:9200/'], verify_certs=True)
@@ -8,7 +11,8 @@ es = Elasticsearch(['http://elastic:9200/'], verify_certs=True)
 
 @app.route('/')
 def hello():
-    return "Hello, World"
+    response = {"message": "Hello, World"}
+    return dumps(response), 200, get_headers(response)
 
 
 @app.route('/elastic')
