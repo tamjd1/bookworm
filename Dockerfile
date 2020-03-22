86c6c7bb58c044b211aa2ddfe0e3d71e2c248623
alka2 <>
2020-03-22 04:20:06 -0400

    added API functionalities

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,9 @@
-FROM python:3.7-alpine
-
+FROM python:3.8-alpine
 WORKDIR /code
-
 ENV FLASK_APP app.py
 ENV FLASK_RUN_HOST 0.0.0.0
-
-RUN apk add --no-cache gcc g++ musl-dev linux-headers
-
+RUN apk add --no-cache gcc g++ musl-dev linux-headers python-dev postgresql-dev
 COPY requirements.txt requirements.txt
-
 RUN pip install -r requirements.txt
-
 COPY . .
 CMD ["flask", "run"]
---
+++ scripts/database.py
@@ -0,0 +1,31 @@
+"""Database management"""
+
+import psycopg2
+
+from scripts.config import DatabaseConfig
+
+
+class DatabaseConnector:
+    """Manage connection to a PostgreSQL database"""
+
+    def __init__(self):
+        self.con = None
+
+    def initialize_connection(self, db_config: DatabaseConfig):
+        try:
+            self.con = psycopg2.connect(
+                host=db_config.host,
+                port=db_config.port,
+                dbname=db_config.db_name,
+                user=db_config.user,
+                password=db_config.password,
+                options=f'-c search_path={db_config.schema}'
+            )
+        except psycopg2.DatabaseError as ex:
+            raise RuntimeError('Database error', ex)
+
+    def close_connection(self):
+        try:
+            self.con.close()
+        except Exception:
+            raise RuntimeError('Database is now unavailable, please try again later')
